48e4bd24e66aec16856f17cab43d50426e362234
njiti <kamaukan77@gmail.com>
2023-12-04 01:34:36 +0300

    created search screen and search bars for filtering

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,10 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../utils/app_layout.dart';
+import '../utils/app_styles.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Search"));
+    final size = AppLayout.getSize(context);
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        children: [
+          const Gap(40),
+          Text("What are\nyou looking for?", style: Styles.headLineStyle1.copyWith(fontSize: 35)),
+          const Gap(20),
+          Container(
+            padding: EdgeInsets.all(3.5),
+            child: Row(
+              children: [
+                /*
+                airline tickets
+                 */
+                Container(
+                  width: size.width*.44,
+                  padding: EdgeInsets.symmetric(vertical: 7),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+                    color: Colors.white,
+                  ),
+                  child: Center(child: Text("Airline Tickets")),
+                ),
+              ],
+             ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: const Color(0xFFF4F6FD),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
